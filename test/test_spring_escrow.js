f6500af42b64738f6b345effe4e1a1ed131bf0fe
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-05-03 10:51:13 -0700

    Modify test to use coinbase account

--- test/test_spring_escrow.js
+++ test/test_spring_escrow.js
@@ -12,11 +12,11 @@ contract('SpringEscrow', (accounts) => {
   // ----------------------------------------------------------------------
 
   it("should verify signer's signature", async () => {
-    const account = web3.eth.accounts[1]
+    const account = web3.eth.accounts[0]
     const msg = 'Hello World'
 
     const h = web3.sha3(msg)
-    const sig = web3.eth.sign(web3.eth.accounts[1], h).slice(2)
+    const sig = web3.eth.sign(web3.eth.accounts[0], h).slice(2)
     const r = `0x${sig.slice(0, 64)}`
     const s = `0x${sig.slice(64, 128)}`
     const v = web3.toDecimal(sig.slice(128, 130)) + 27
