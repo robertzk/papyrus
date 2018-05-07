269b6a40f7696c636a68eaf34c70a3345cf5a957
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-05-07 14:47:39 -0500

    add visibility to fix warning

--- contracts/SpringEscrow.sol
+++ contracts/SpringEscrow.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.23;
 
 contract SpringEscrow {
 
-  function verifySignature(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) view returns (address) {
+  function verifySignature(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) pure public returns (address) {
     address signer = ecrecover(_message, _v, _r, _s);
     return signer;
   }
