3c6199d6891a41ac2b7209b519a2867a9267809b
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-05-07 14:55:36 -0500

    add AttestationRegistry to deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 var Migrations = artifacts.require("./Migrations.sol");
 var SpringEscrow = artifacts.require("./SpringEscrow.sol");
+var AttestationRegistry = artifacts.require("./AttestationRegistry.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(SpringEscrow);
+  deployer.deploy(AttestationRegistry);
 };
