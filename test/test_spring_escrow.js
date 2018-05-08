2a945fa495b5912fd5c6a8e018d8e6653301f7e3
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-05-08 16:38:50 -0500

    modify test to use web3.eth.sign instead of web3.eth.accounts.sign

--- test/test_spring_escrow.js
+++ test/test_spring_escrow.js
@@ -3,8 +3,6 @@ const SpringEscrow = artifacts.require("./SpringEscrow.sol")
 const Web3Beta = require('web3')
 const web3 = new Web3Beta(new Web3Beta.providers.HttpProvider("http://localhost:8545"))
 
-const PRIVATE_KEY="0x789610cb2d3bd970aadc1f9fb119f85ebdc25a0cecbdd86d9af28901c12b98ae"
-
 contract('SpringEscrow', (accounts) => {
   const coinbase = accounts[0]
   let instance
@@ -19,9 +17,17 @@ contract('SpringEscrow', (accounts) => {
 
   it("should verify signer's signature", async () => {
     const msg = 'Hello World'
-    const data = "\x19Ethereum Signed Message:\n" + msg.length + msg
-    var signature = web3.eth.accounts.sign(data, PRIVATE_KEY)
-    const signerAddress = await instance.verifySignature(signature.messageHash, signature.v, signature.r, signature.s)
+
+    var hashMsg = web3.utils.sha3(msg)
+    var signature = await web3.eth.sign(hashMsg, coinbase)
+
+    signature = signature.substr(2, signature.length)
+    const r = '0x' + signature.substr(0, 64)
+    const s = '0x' + signature.substr(64, 128)
+    const v = web3.utils.toDecimal('0x' + signature.substr(128, 130)) + 27
+
+    const signerAddress = await instance.verifySignature(hashMsg, v, r, s)
+
     assert.equal(signerAddress, coinbase, "Address of signer should be equal to sender's account")
   })
 });
