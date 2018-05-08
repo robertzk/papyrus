709de7b13ab898a5e94d16d43ad66782009479b3
Fabrice Cheng <fabrice.cheng@gmail.com>
2018-05-08 16:38:23 -0500

    fix SpringEscrow contract to match web3.eth.sign instead of web3.eth.accounts.sign

--- contracts/SpringEscrow.sol
+++ contracts/SpringEscrow.sol
@@ -2,8 +2,11 @@ pragma solidity ^0.4.23;
 
 contract SpringEscrow {
 
-  function verifySignature(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) pure public returns (address) {
-    address signer = ecrecover(_message, _v, _r, _s);
-    return signer;
+  function verifySignature(bytes32 _hashMessage, uint8 _v, bytes32 _r, bytes32 _s) pure public returns (address) {
+    bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+    bytes32 prefixedHash = keccak256(prefix, _hashMessage);
+    address addr = ecrecover(prefixedHash, _v, _r, _s);
+
+    return addr;
   }
 }
